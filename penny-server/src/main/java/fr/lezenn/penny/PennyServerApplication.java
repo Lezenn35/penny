2dbb33c3b2ee164e759e8badd9c33fdc39407eb2
Gireg Lezoraine <gireg.lezoraine2022@campus-eni.fr>
2022-04-22 13:45:22 +0200

    Update

---
+++ penny-server/src/main/java/fr/lezenn/penny/PennyServerApplication.java
@@ -0,0 +1,13 @@
+package fr.lezenn.penny;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PennyServerApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(PennyServerApplication.class, args);
+    }
+
+}
---
+++ penny-server/src/test/java/fr/lezenn/penny/PennyServerApplicationTests.java
@@ -0,0 +1,13 @@
+package fr.lezenn.penny;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PennyServerApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
